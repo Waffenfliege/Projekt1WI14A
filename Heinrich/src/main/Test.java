eb4855c849fb067870622e29592b4b6f38b4de9d
UltimateCuB3R <jansauerland@gmx.de>
2017-01-23 14:07:22 +0100

    Formatierung ist jetzt in den Settings hinterlegt.

--- Heinrich/src/main/Test.java
+++ Heinrich/src/main/Test.java
@@ -1,10 +1,11 @@
 package main;
 
-public class Test {
-	
-	int i =0;
+public class Test
+{
 
-   // Hallo Ich bin ein Test
-	
+	int i = 0;
+
+	// Hallo Ich bin ein Test
+
 	// Test - Jan hat's auch mal geschafft!
 }
