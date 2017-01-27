9c3462c5dbeb7f4ae2234066fb80820021997166
j <jonathan.klopfer@student.dhbw-vs.de>
2017-01-27 13:47:24 +0100

    DataHandler method update

--- Heinrich/src/data/DataHandler.java
+++ Heinrich/src/data/DataHandler.java
@@ -13,12 +13,28 @@ public class DataHandler
 		classes = new ArrayList<StatisticClass>();
 	}
 	
-	public static ArrayList<StatisticClass> getAllClasses(){
+	public static ArrayList<StatisticClass> getList(){
 		return classes;
 	}
 	
 	public static StatisticClass getElement(int index){
 		return classes.get(index);
 		
+	}
+	
+	public static void updateListItem(int index, StatisticClassValue lowerValue, StatisticClassValue upperValue, int absoluteOccurence) {
+		classes.get(index).setLowerValue(lowerValue);
+		classes.get(index).setUpperValue(upperValue);
+		classes.get(index).setAbsoluteOccurences(absoluteOccurence);
+	}
+	
+	public static void put(StatisticClassValue lowerValue, StatisticClassValue upperValue, int absoluteOccurence){
+		classes.add(new StatisticClass(lowerValue, upperValue, absoluteOccurence));	
+	}
+	
+	public static int getSampleSize(){
+		int sampleSize = 0;
+		
+		return sampleSize;
 	}
 }
