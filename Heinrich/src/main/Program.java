1588f8a8555c03ec0cf30813b5030d5404c94893
Mathias Engmann <jan.mathias.engmann@gmail.com>
2017-01-27 14:20:39 +0100

    Klassenmitten

--- Heinrich/src/main/Program.java
+++ Heinrich/src/main/Program.java
@@ -17,22 +17,28 @@ public class Program
 		//######################TEST###################
 		
 		//float relativeOccurences[] = LogicHandler.getRelativeOccurences(dataHandler.getList(), dataHandler.getSampleSize());
-		float relativeOccurences[] = LogicHandler.getRelativeOccurences(dataHandler.getList(), 7);
-		float classMiddles[] = LogicHandler.getClassMiddles(dataHandler.getList());
+		try{
+			float relativeOccurences[] = LogicHandler.getRelativeOccurences(dataHandler.getList(), 0);
+			float classMiddles[] = LogicHandler.getClassMiddles(dataHandler.getList());
 
-		
-		for(int i=0; i<dataHandler.getList().size();i++){
-		
-			float absoluteOccurence = dataHandler.getElement(i).getAbsoluteOccurences();
-			System.out.println("Daten für Klasse "+ (i+1) + ": ");
 			
-			System.out.println("--- absolute Häufigkeit: " + absoluteOccurence);
-			System.out.println("--- relative Häufigkeit: " + relativeOccurences[i]);
-			System.out.println("--- Klassenmitte: " + classMiddles[i]);
+			for(int i=0; i<dataHandler.getList().size();i++){
+			
+				float absoluteOccurence = dataHandler.getElement(i).getAbsoluteOccurences();
+				System.out.println("Daten für Klasse "+ (i+1) + ": ");
+				
+				System.out.println("--- absolute Häufigkeit: " + absoluteOccurence);
+				System.out.println("--- relative Häufigkeit: " + relativeOccurences[i]);
+				System.out.println("--- Klassenmitte: " + classMiddles[i]);
+			}
+			
+			
+			float median = LogicHandler.median(dataHandler.getList());
+		
+			
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-		
-		
-		float median = LogicHandler.median(dataHandler.getList());
 	
 	}
 
