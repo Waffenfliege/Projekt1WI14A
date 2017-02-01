5d12216c749c0796358069f26d6c09138bbc1cad
UltimateCuB3R <jansauerland@gmx.de>
2017-02-01 14:10:24 +0100

    Ergebnis-Setter hinzugefügt

--- Heinrich/src/view/OutputPanel.java
+++ Heinrich/src/view/OutputPanel.java
@@ -27,28 +27,10 @@ public class OutputPanel extends JPanel
 			empiricContainer, empiricPanel, histogramPanel, diagramContainer;
 	private ButtonContainer oButtonContainer, hButtonContainer, eButtonContainer;
 	private JTabbedPane outputTabbedPane;
-	private JLabel resultHeaderLabel;
-	private JLabel middleLabel1;
-	private JLabel medianLabel1;
-	private JLabel quantileLabel1;
-	private JLabel absoluteLabel1;
-	private JLabel standardLabel1;
-	private JLabel varianceLabel1;
-	private JLabel giniLabel1;
-	private JLabel middleLabel2;
-	private JLabel medianLabel2;
-	private JLabel quantileLabel2;
-	private JLabel absoluteLabel2;
-	private JLabel varianceLabel2;
-	private JLabel standardLabel2;
-	private JLabel giniLabel2;
-	private JLabel middleLabel3;
-	private JLabel medianLabel3;
-	private JLabel quantileLabel3;
-	private JLabel absoluteLabel3;
-	private JLabel varianceLabel3;
-	private JLabel standardLabel3;
-	private JLabel giniLabel3;
+	private JLabel resultHeaderLabel, middleLabel1, medianLabel1, quantileLabel1, absoluteLabel1, standardLabel1,
+			varianceLabel1, giniLabel1, middleLabel2, medianLabel2, quantileLabel2, absoluteLabel2, varianceLabel2,
+			standardLabel2, giniLabel2, middleLabel3, medianLabel3, quantileLabel3, absoluteLabel3, varianceLabel3,
+			standardLabel3, giniLabel3;
 
 	/**
 	 * Create the panel.
@@ -101,12 +83,12 @@ public class OutputPanel extends JPanel
 		resultValuePanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		resultPanel.add(resultValuePanel, BorderLayout.CENTER);
 		resultValuePanel.setLayout(new GridLayout(7, 3, 0, 0));
-		
+
 		middleLabel1 = new JLabel("Mittelwert");
 		middleLabel1.setVerticalTextPosition(SwingConstants.TOP);
 		middleLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(middleLabel1);
-		
+
 		middleLabel2 = new JLabel("");
 		middleLabel2.setVerticalAlignment(SwingConstants.TOP);
 		middleLabel2.setSize(new Dimension(100, 35));
@@ -115,15 +97,15 @@ public class OutputPanel extends JPanel
 		middleLabel2.setMinimumSize(new Dimension(100, 35));
 		middleLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/Mittel.png")));
 		resultValuePanel.add(middleLabel2);
-		
+
 		middleLabel3 = new JLabel("value");
 		middleLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(middleLabel3);
-		
+
 		medianLabel1 = new JLabel("Median");
 		medianLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(medianLabel1);
-		
+
 		medianLabel2 = new JLabel("");
 		medianLabel2.setVerticalAlignment(SwingConstants.TOP);
 		medianLabel2.setSize(new Dimension(100, 35));
@@ -132,15 +114,15 @@ public class OutputPanel extends JPanel
 		medianLabel2.setMaximumSize(new Dimension(100, 35));
 		medianLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/Median.png")));
 		resultValuePanel.add(medianLabel2);
-		
+
 		medianLabel3 = new JLabel("value");
 		medianLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(medianLabel3);
-		
+
 		quantileLabel1 = new JLabel("Quantile");
 		quantileLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(quantileLabel1);
-		
+
 		quantileLabel2 = new JLabel("");
 		quantileLabel2.setVerticalTextPosition(SwingConstants.TOP);
 		quantileLabel2.setVerticalAlignment(SwingConstants.TOP);
@@ -150,15 +132,15 @@ public class OutputPanel extends JPanel
 		quantileLabel2.setMaximumSize(new Dimension(100, 145));
 		quantileLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/Quantile.png")));
 		resultValuePanel.add(quantileLabel2);
-		
+
 		quantileLabel3 = new JLabel("value");
 		quantileLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(quantileLabel3);
-		
+
 		absoluteLabel1 = new JLabel("Absolut");
 		absoluteLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(absoluteLabel1);
-		
+
 		absoluteLabel2 = new JLabel("");
 		absoluteLabel2.setSize(new Dimension(100, 35));
 		absoluteLabel2.setPreferredSize(new Dimension(100, 35));
@@ -167,15 +149,15 @@ public class OutputPanel extends JPanel
 		absoluteLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/absolute.png")));
 		absoluteLabel2.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(absoluteLabel2);
-		
+
 		absoluteLabel3 = new JLabel("value");
 		absoluteLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(absoluteLabel3);
-		
+
 		varianceLabel1 = new JLabel("Varianz");
 		varianceLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(varianceLabel1);
-		
+
 		varianceLabel2 = new JLabel("");
 		varianceLabel2.setVerticalAlignment(SwingConstants.TOP);
 		varianceLabel2.setSize(new Dimension(100, 35));
@@ -184,15 +166,15 @@ public class OutputPanel extends JPanel
 		varianceLabel2.setMaximumSize(new Dimension(100, 35));
 		varianceLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/variance.png")));
 		resultValuePanel.add(varianceLabel2);
-		
+
 		varianceLabel3 = new JLabel("value");
 		varianceLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(varianceLabel3);
-		
+
 		standardLabel1 = new JLabel("Standard");
 		standardLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(standardLabel1);
-		
+
 		standardLabel2 = new JLabel("");
 		standardLabel2.setVerticalAlignment(SwingConstants.TOP);
 		standardLabel2.setSize(new Dimension(100, 35));
@@ -201,15 +183,15 @@ public class OutputPanel extends JPanel
 		standardLabel2.setMaximumSize(new Dimension(100, 35));
 		standardLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/standard.png")));
 		resultValuePanel.add(standardLabel2);
-		
+
 		standardLabel3 = new JLabel("value");
 		standardLabel3.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(standardLabel3);
-		
+
 		giniLabel1 = new JLabel("Gini");
 		giniLabel1.setVerticalAlignment(SwingConstants.TOP);
 		resultValuePanel.add(giniLabel1);
-		
+
 		giniLabel2 = new JLabel("");
 		giniLabel2.setVerticalAlignment(SwingConstants.TOP);
 		giniLabel2.setSize(new Dimension(100, 35));
@@ -218,143 +200,127 @@ public class OutputPanel extends JPanel
 		giniLabel2.setMaximumSize(new Dimension(100, 35));
 		giniLabel2.setIcon(new ImageIcon(OutputPanel.class.getResource("/view/gini.png")));
 		resultValuePanel.add(giniLabel2);
-		
+
 		giniLabel3 = new JLabel("value");
 		resultValuePanel.add(giniLabel3);
 
-		/*JPanel resultMiddlePanel = new JPanel();
-		resultMiddlePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout flowLayout_1 = (FlowLayout) resultMiddlePanel.getLayout();
-		flowLayout_1.setHgap(10);
-		flowLayout_1.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultMiddlePanel);
-
-		JLabel resultMiddleLabel = new JLabel("Mittelwert");
-		resultMiddlePanel.add(resultMiddleLabel);
-
-		JLabel lblX = new JLabel("x");
-		resultMiddlePanel.add(lblX);
-
-		JLabel label = new JLabel("=");
-		resultMiddlePanel.add(label);
-
-		JLabel lblValue = new JLabel("value");
-		resultMiddlePanel.add(lblValue);
-
-		JPanel resultMedianPanel = new JPanel();
-		resultMedianPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout flowLayout_2 = (FlowLayout) resultMedianPanel.getLayout();
-		flowLayout_2.setHgap(10);
-		flowLayout_2.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultMedianPanel);
-
-		JLabel lblNewLabel_1 = new JLabel("Median");
-		resultMedianPanel.add(lblNewLabel_1);
-
-		JLabel label_1 = new JLabel("x");
-		resultMedianPanel.add(label_1);
-
-		JLabel label_2 = new JLabel("=");
-		resultMedianPanel.add(label_2);
-
-		JLabel label_6 = new JLabel("value");
-		resultMedianPanel.add(label_6);
-
-		JPanel resultQuantilePanel = new JPanel();
-		resultQuantilePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout fl_resultQuantilePanel = (FlowLayout) resultQuantilePanel.getLayout();
-		fl_resultQuantilePanel.setHgap(10);
-		fl_resultQuantilePanel.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultQuantilePanel);
-
-		JLabel lblQuantile = new JLabel("Quantile");
-		resultQuantilePanel.add(lblQuantile);
-
-		JLabel label_3 = new JLabel("x");
-		resultQuantilePanel.add(label_3);
-
-		JLabel label_4 = new JLabel("=");
-		resultQuantilePanel.add(label_4);
-
-		JLabel label_5 = new JLabel("value");
-		resultQuantilePanel.add(label_5);
-
-		JPanel resultDeviationPanel = new JPanel();
-		resultDeviationPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout fl_resultDeviationPanel = (FlowLayout) resultDeviationPanel.getLayout();
-		fl_resultDeviationPanel.setHgap(10);
-		fl_resultDeviationPanel.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultDeviationPanel);
-
-		JLabel lblAbsoluteAbweichung = new JLabel("Absolute Abweichung");
-		resultDeviationPanel.add(lblAbsoluteAbweichung);
-
-		JLabel label_8 = new JLabel("x");
-		resultDeviationPanel.add(label_8);
-
-		JLabel label_9 = new JLabel("=");
-		resultDeviationPanel.add(label_9);
-
-		JLabel label_10 = new JLabel("value");
-		resultDeviationPanel.add(label_10);
-
-		JPanel resultVariancePanel = new JPanel();
-		resultVariancePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout fl_resultVariancePanel = (FlowLayout) resultVariancePanel.getLayout();
-		fl_resultVariancePanel.setHgap(10);
-		fl_resultVariancePanel.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultVariancePanel);
-
-		JLabel lblVarianz = new JLabel("Varianz");
-		resultVariancePanel.add(lblVarianz);
-
-		JLabel label_11 = new JLabel("x");
-		resultVariancePanel.add(label_11);
-
-		JLabel label_12 = new JLabel("=");
-		resultVariancePanel.add(label_12);
-
-		JLabel label_13 = new JLabel("value");
-		resultVariancePanel.add(label_13);
-
-		JPanel resultStandardPanel = new JPanel();
-		resultStandardPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout fl_resultStandardPanel = (FlowLayout) resultStandardPanel.getLayout();
-		fl_resultStandardPanel.setHgap(10);
-		fl_resultStandardPanel.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultStandardPanel);
-
-		JLabel lblStandardabweichung_1 = new JLabel("Standardabweichung");
-		resultStandardPanel.add(lblStandardabweichung_1);
-
-		JLabel label_14 = new JLabel("x");
-		resultStandardPanel.add(label_14);
-
-		JLabel label_15 = new JLabel("=");
-		resultStandardPanel.add(label_15);
-
-		JLabel label_16 = new JLabel("value");
-		resultStandardPanel.add(label_16);
-
-		JPanel resultGiniPanel = new JPanel();
-		resultGiniPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		FlowLayout fl_resultGiniPanel = (FlowLayout) resultGiniPanel.getLayout();
-		fl_resultGiniPanel.setHgap(10);
-		fl_resultGiniPanel.setAlignment(FlowLayout.LEFT);
-		resultValuePanel.add(resultGiniPanel);
-
-		JLabel lblGiniKoeffizient = new JLabel("Gini Koeffizient");
-		resultGiniPanel.add(lblGiniKoeffizient);
-
-		JLabel label_17 = new JLabel("x");
-		resultGiniPanel.add(label_17);
-
-		JLabel label_18 = new JLabel("=");
-		resultGiniPanel.add(label_18);
-
-		JLabel label_19 = new JLabel("value");
-		resultGiniPanel.add(label_19);
-		*/
+		/*
+		 * JPanel resultMiddlePanel = new JPanel();
+		 * resultMiddlePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout flowLayout_1 = (FlowLayout) resultMiddlePanel.getLayout();
+		 * flowLayout_1.setHgap(10); flowLayout_1.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultMiddlePanel);
+		 * 
+		 * JLabel resultMiddleLabel = new JLabel("Mittelwert");
+		 * resultMiddlePanel.add(resultMiddleLabel);
+		 * 
+		 * JLabel lblX = new JLabel("x"); resultMiddlePanel.add(lblX);
+		 * 
+		 * JLabel label = new JLabel("="); resultMiddlePanel.add(label);
+		 * 
+		 * JLabel lblValue = new JLabel("value");
+		 * resultMiddlePanel.add(lblValue);
+		 * 
+		 * JPanel resultMedianPanel = new JPanel();
+		 * resultMedianPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout flowLayout_2 = (FlowLayout) resultMedianPanel.getLayout();
+		 * flowLayout_2.setHgap(10); flowLayout_2.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultMedianPanel);
+		 * 
+		 * JLabel lblNewLabel_1 = new JLabel("Median");
+		 * resultMedianPanel.add(lblNewLabel_1);
+		 * 
+		 * JLabel label_1 = new JLabel("x"); resultMedianPanel.add(label_1);
+		 * 
+		 * JLabel label_2 = new JLabel("="); resultMedianPanel.add(label_2);
+		 * 
+		 * JLabel label_6 = new JLabel("value"); resultMedianPanel.add(label_6);
+		 * 
+		 * JPanel resultQuantilePanel = new JPanel();
+		 * resultQuantilePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout fl_resultQuantilePanel = (FlowLayout)
+		 * resultQuantilePanel.getLayout(); fl_resultQuantilePanel.setHgap(10);
+		 * fl_resultQuantilePanel.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultQuantilePanel);
+		 * 
+		 * JLabel lblQuantile = new JLabel("Quantile");
+		 * resultQuantilePanel.add(lblQuantile);
+		 * 
+		 * JLabel label_3 = new JLabel("x"); resultQuantilePanel.add(label_3);
+		 * 
+		 * JLabel label_4 = new JLabel("="); resultQuantilePanel.add(label_4);
+		 * 
+		 * JLabel label_5 = new JLabel("value");
+		 * resultQuantilePanel.add(label_5);
+		 * 
+		 * JPanel resultDeviationPanel = new JPanel();
+		 * resultDeviationPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout fl_resultDeviationPanel = (FlowLayout)
+		 * resultDeviationPanel.getLayout();
+		 * fl_resultDeviationPanel.setHgap(10);
+		 * fl_resultDeviationPanel.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultDeviationPanel);
+		 * 
+		 * JLabel lblAbsoluteAbweichung = new JLabel("Absolute Abweichung");
+		 * resultDeviationPanel.add(lblAbsoluteAbweichung);
+		 * 
+		 * JLabel label_8 = new JLabel("x"); resultDeviationPanel.add(label_8);
+		 * 
+		 * JLabel label_9 = new JLabel("="); resultDeviationPanel.add(label_9);
+		 * 
+		 * JLabel label_10 = new JLabel("value");
+		 * resultDeviationPanel.add(label_10);
+		 * 
+		 * JPanel resultVariancePanel = new JPanel();
+		 * resultVariancePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout fl_resultVariancePanel = (FlowLayout)
+		 * resultVariancePanel.getLayout(); fl_resultVariancePanel.setHgap(10);
+		 * fl_resultVariancePanel.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultVariancePanel);
+		 * 
+		 * JLabel lblVarianz = new JLabel("Varianz");
+		 * resultVariancePanel.add(lblVarianz);
+		 * 
+		 * JLabel label_11 = new JLabel("x"); resultVariancePanel.add(label_11);
+		 * 
+		 * JLabel label_12 = new JLabel("="); resultVariancePanel.add(label_12);
+		 * 
+		 * JLabel label_13 = new JLabel("value");
+		 * resultVariancePanel.add(label_13);
+		 * 
+		 * JPanel resultStandardPanel = new JPanel();
+		 * resultStandardPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		 * FlowLayout fl_resultStandardPanel = (FlowLayout)
+		 * resultStandardPanel.getLayout(); fl_resultStandardPanel.setHgap(10);
+		 * fl_resultStandardPanel.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultStandardPanel);
+		 * 
+		 * JLabel lblStandardabweichung_1 = new JLabel("Standardabweichung");
+		 * resultStandardPanel.add(lblStandardabweichung_1);
+		 * 
+		 * JLabel label_14 = new JLabel("x"); resultStandardPanel.add(label_14);
+		 * 
+		 * JLabel label_15 = new JLabel("="); resultStandardPanel.add(label_15);
+		 * 
+		 * JLabel label_16 = new JLabel("value");
+		 * resultStandardPanel.add(label_16);
+		 * 
+		 * JPanel resultGiniPanel = new JPanel(); resultGiniPanel.setBorder(new
+		 * LineBorder(Color.LIGHT_GRAY)); FlowLayout fl_resultGiniPanel =
+		 * (FlowLayout) resultGiniPanel.getLayout();
+		 * fl_resultGiniPanel.setHgap(10);
+		 * fl_resultGiniPanel.setAlignment(FlowLayout.LEFT);
+		 * resultValuePanel.add(resultGiniPanel);
+		 * 
+		 * JLabel lblGiniKoeffizient = new JLabel("Gini Koeffizient");
+		 * resultGiniPanel.add(lblGiniKoeffizient);
+		 * 
+		 * JLabel label_17 = new JLabel("x"); resultGiniPanel.add(label_17);
+		 * 
+		 * JLabel label_18 = new JLabel("="); resultGiniPanel.add(label_18);
+		 * 
+		 * JLabel label_19 = new JLabel("value"); resultGiniPanel.add(label_19);
+		 */
 		diagramContainer = new JPanel();
 		diagramContainer.setBorder(new LineBorder(new Color(0, 0, 0)));
 		resultContainer.add(diagramContainer);
@@ -394,6 +360,60 @@ public class OutputPanel extends JPanel
 	public OutputPanel get()
 	{
 		return this;
+	}
+
+	/**
+	 * 
+	 * @param median
+	 */
+	public void setMedian(float median)
+	{
+		medianLabel3.setText("" + median);
+	}
+
+	/**
+	 * 
+	 * @param middle
+	 */
+	public void setMiddle(float middle)
+	{
+		middleLabel3.setText("" + middle);
+	}
+
+	/**
+	 * 
+	 * @param variance
+	 */
+	public void setVariance(float variance)
+	{
+		varianceLabel3.setText("" + variance);
+	}
+
+	/**
+	 * 
+	 * @param deviation
+	 */
+	public void setAbsoluteDeviation(float deviation)
+	{
+		absoluteLabel3.setText("" + deviation);
+	}
+
+	/**
+	 * 
+	 * @param deviation
+	 */
+	public void setStandardDeviation(float deviation)
+	{
+		standardLabel3.setText("" + deviation);
+	}
+
+	/**
+	 * 
+	 * @param gini
+	 */
+	public void setGini(float gini)
+	{
+		giniLabel3.setText("" + gini);
 	}
 
 	/*
