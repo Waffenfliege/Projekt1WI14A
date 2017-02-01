ebe9821d52aba64da9afbffc2b5271d0d6b96710
LukeWI <lukas_moser@online.de>
2017-02-01 10:27:37 +0100

    Validation

--- Heinrich/src/view/ButtonContainer.java
+++ Heinrich/src/view/ButtonContainer.java
@@ -15,7 +15,8 @@ import data.StatisticClassValue;
 import java.awt.Color;
 
 @SuppressWarnings("serial")
-public class ButtonContainer extends JPanel {
+public class ButtonContainer extends JPanel
+{
 	private JButton changeValueButton, newCalculationButton, lastClassButton, nextClassButton, calculateButton;
 	private FlowLayout flowLayout;
 	private int index = 0;
@@ -23,9 +24,11 @@ public class ButtonContainer extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public ButtonContainer(boolean output) {
+	public ButtonContainer(boolean output)
+	{
 		super();
-		if (output) {
+		if (output)
+		{
 			setBorder(new LineBorder(new Color(0, 0, 0)));
 			flowLayout = (FlowLayout) this.getLayout();
 			flowLayout.setHgap(75);
@@ -38,7 +41,8 @@ public class ButtonContainer extends JPanel {
 			newCalculationButton = new JButton("Neue Berechnung");
 			newCalculationButton.addActionListener(newCalculationAction);
 			add(newCalculationButton);
-		} else if (!output) {
+		} else if (!output)
+		{
 			setBorder(null);
 			setBackground(Color.WHITE);
 			setLayout(new FlowLayout(FlowLayout.CENTER, 25, 15));
@@ -57,85 +61,118 @@ public class ButtonContainer extends JPanel {
 		}
 	}
 
-	private ActionListener newCalculationAction = new ActionListener() {
-		public void actionPerformed(ActionEvent actionEvent) {
+	private ActionListener newCalculationAction = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent actionEvent)
+		{
 			// TODO: Action bei Button "Neue Berechnung": Input-Panel komplett
 			// neu aufrufen
 			String dialogString = InputDialog.startNewDialog();
-			if (dialogString.equals("Ja")) {
+			if (dialogString.equals("Ja"))
+			{
 				MainFrame.switchToInputPanel(true);
 			}
 		}
 	};
 
-	private ActionListener changeValueAction = new ActionListener() {
-		public void actionPerformed(ActionEvent actionEvent) {
+	private ActionListener changeValueAction = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent actionEvent)
+		{
 			// TODO: Action bei Button "Werte ändern": Input-Panel mit
 			// bestehenden Daten aufrufen
 			MainFrame.switchToInputPanel(false);
 		}
 	};
 
-	private ActionListener lastClassAction = new ActionListener() {
-		public void actionPerformed(ActionEvent actionEvent) {
-			// TODO: Action bei Button "vorherige Klasse": Werte überprüfen,
-			// abspeichern, Eingabefelder leeren
-			
-			float lowerValue = Float.parseFloat(InputPanel.getLeftClassBorderField());
-			float upperValue = Float.parseFloat(InputPanel.getRightClassBorderField());
-			ClampType lowerClampType = ClampType.INCLUSIVE;
-			ClampType upperClampType = ClampType.EXCLUSIVE;
-			int absoluteOccurence = Integer.parseInt(InputPanel.getQuantityField());
+	private ActionListener lastClassAction = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent actionEvent)
+		{
 
-			try {
-				DataHandler.receiveData(new StatisticClassValue(lowerValue, lowerClampType),
-						new StatisticClassValue(upperValue, upperClampType), absoluteOccurence, index);
-				InputPanel.resetFields();
-				index--;
-			} catch (Exception e) {
-				e.printStackTrace();
+			if (isValid(InputPanel.getLeftClassBorderField()) && isValid(InputPanel.getRightClassBorderField())
+					&& isValid(InputPanel.getQuantityField()))
+			{
+				float lowerValue = Float.parseFloat(InputPanel.getLeftClassBorderField());
+				float upperValue = Float.parseFloat(InputPanel.getRightClassBorderField());
+				ClampType lowerClampType = ClampType.INCLUSIVE;
+				ClampType upperClampType = ClampType.EXCLUSIVE;
+				int absoluteOccurence = Integer.parseInt(InputPanel.getQuantityField());
+
+				try
+				{
+					DataHandler.receiveData(new StatisticClassValue(lowerValue, lowerClampType),
+							new StatisticClassValue(upperValue, upperClampType), absoluteOccurence, index);
+					InputPanel.resetFields();
+					index--;
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
 			}
-			
 			MainFrame.getInputPanel().revalidate();
 			MainFrame.getInputPanel().repaint();
 		}
 	};
 
-	private ActionListener nextClassAction = new ActionListener() {
-		public void actionPerformed(ActionEvent actionEvent) {
-			// TODO: Action bei Button "nächste Klasse": Werte überprüfen,
-			// abspeichern, Eingabefelder leeren
+	private ActionListener nextClassAction = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent actionEvent)
+		{
+			if (isValid(InputPanel.getLeftClassBorderField()) && isValid(InputPanel.getRightClassBorderField())
+					&& isValid(InputPanel.getQuantityField()))
+			{
+				float lowerValue = Float.parseFloat(InputPanel.getLeftClassBorderField());
+				float upperValue = Float.parseFloat(InputPanel.getRightClassBorderField());
+				ClampType lowerClampType = ClampType.INCLUSIVE;
+				ClampType upperClampType = ClampType.EXCLUSIVE;
+				int absoluteOccurence = Integer.parseInt(InputPanel.getQuantityField());
 
-			// Folgende Werte aus den Texboxen / Klammerbuttons auslesen
-			float lowerValue = Float.parseFloat(InputPanel.getLeftClassBorderField());
-			float upperValue = Float.parseFloat(InputPanel.getRightClassBorderField());
-			ClampType lowerClampType = ClampType.INCLUSIVE;
-			ClampType upperClampType = ClampType.EXCLUSIVE;
-			int absoluteOccurence = Integer.parseInt(InputPanel.getQuantityField());
-
-			try {
-				DataHandler.receiveData(new StatisticClassValue(lowerValue, lowerClampType),
-						new StatisticClassValue(upperValue, upperClampType), absoluteOccurence, index);
-				InputPanel.resetFields();
-				index++;
-			} catch (Exception e) {
-				e.printStackTrace();
+				try
+				{
+					DataHandler.receiveData(new StatisticClassValue(lowerValue, lowerClampType),
+							new StatisticClassValue(upperValue, upperClampType), absoluteOccurence, index);
+					InputPanel.resetFields();
+					index++;
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
 			}
-
 			MainFrame.getInputPanel().revalidate();
 			MainFrame.getInputPanel().repaint();
 		}
 	};
 
-	private ActionListener calculateAction = new ActionListener() {
-		public void actionPerformed(ActionEvent actionEvent) {
-			// TODO: Action bei Button "Berechnen": Werte überprüfen,
-			// abspeichern, OutputPanel aufrufen, Berechnete Werte anzeigen,
-			// Diagramme berechnen (= Werte an LogicHandler übergeben)
-			String dialogString = InputDialog.startZDialog();
-			// variable z = Double.parseDouble(dialogString);
+	private ActionListener calculateAction = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent actionEvent)
+		{
+
+			if (isValid(InputPanel.getLeftClassBorderField()) && isValid(InputPanel.getRightClassBorderField())
+					&& isValid(InputPanel.getQuantityField()))
+			{
+				// TODO: Action bei Button "Berechnen": Werte überprüfen,
+
+				// abspeichern, OutputPanel aufrufen, Berechnete Werte anzeigen,
+				// Diagramme berechnen (= Werte an LogicHandler übergeben)
+				String dialogString = InputDialog.startZDialog();
+
+				// variable z = Double.parseDouble(dialogString);
+			}
 			MainFrame.switchToOutputPanel();
 		}
 	};
-}
 
+	private boolean isValid(String input)
+	{
+		try
+		{
+			Double.parseDouble(input);
+			return true;
+		} catch (NumberFormatException e)
+		{
+			return false;
+		}
+	}
+}
