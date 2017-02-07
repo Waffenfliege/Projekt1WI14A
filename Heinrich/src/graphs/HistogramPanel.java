aacc86ff2b993c4265eb942d596c8b7ad4df7bce
Mathias Engmann <jan.mathias.engmann@gmail.com>
2017-02-07 12:48:25 +0100

    Histogramm soweit fertig

--- Heinrich/src/graphs/HistogramPanel.java
+++ Heinrich/src/graphs/HistogramPanel.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+import view.MainFrame;
+
 public class HistogramPanel extends JPanel{
 
 	ArrayList<Rectangle> rectangles;
@@ -75,10 +77,11 @@ public class HistogramPanel extends JPanel{
 		g2.drawLine(origin.getPosX(), origin.getPosY()+chartHeight, origin.getPosX()+chartWidth, origin.getPosY()+chartHeight);
 	
 		if(isDetailed){
+			//########################## MARKERS ###########################
 			//X-AXIS Regular Markers
 			g2.setStroke(new BasicStroke(1));
-			int markerStep = chartWidth/10;
-			for(int i=1; i<10;i++){
+			int markerStep = chartWidth/11;
+			for(int i=1; i<11;i++){
 				int x = origin.getPosX()+ markerStep*i;
 				int y = origin.getPosY()+chartHeight-6;
 				int xTarget =   origin.getPosX()+ markerStep*i;
@@ -86,7 +89,7 @@ public class HistogramPanel extends JPanel{
 				g2.drawLine(x, y, xTarget, yTarget);
 			}
 			
-			//X-AXIS Borders
+			//X-AXIS Markers
 			g2.setStroke(new BasicStroke(3));
 			for(int i=0; i<rectangles.size();i++){
 				int x = origin.getPosX()+positions.get(i).getPosX()+rectangles.get(i).getWidth();
@@ -98,8 +101,8 @@ public class HistogramPanel extends JPanel{
 			
 			//Y-AXIS Regular Markers
 			g2.setStroke(new BasicStroke(1));
-			markerStep = chartHeight/10;
-			for(int i=1; i<10;i++){
+			markerStep = chartHeight/11;
+			for(int i=1; i<11;i++){
 				int x = origin.getPosX()-6;
 				int y = origin.getPosY()+markerStep*i;
 				int xTarget =   origin.getPosX()+6;
@@ -107,7 +110,7 @@ public class HistogramPanel extends JPanel{
 				g2.drawLine(x, y, xTarget, yTarget);
 			}
 			
-			//Y-AXIS Column Borders
+			//Y-AXIS Column Markers
 			g2.setStroke(new BasicStroke(3));
 			for(int i=0; i<rectangles.size();i++){
 				int x = origin.getPosX()-12;
@@ -117,39 +120,58 @@ public class HistogramPanel extends JPanel{
 				g2.drawLine(x, y, xTarget, yTarget);
 			}
 			
-			//Column Labels
+			//########################## LABELS ###########################
+			//Column LABELS
 			for(int i=0; i<rectangles.size();i++){
-				int x = positions.get(i).getPosX()+rectangles.get(i).getWidth()/2;
-				int y = positions.get(i).getPosY()+rectangles.get(i).getHeight()/2;
+				String labelString = "K " + (i+1);
+				int stringWidth = g2.getFontMetrics().stringWidth(labelString);
+				int x = origin.getPosX()+positions.get(i).getPosX()+rectangles.get(i).getWidth()/2-stringWidth/2;
+				int y = origin.getPosY()+positions.get(i).getPosY()+rectangles.get(i).getHeight()/2;
+				
+				System.out.println("Y: " + y);
+				if(	rectangles.get(i).getHeight()<30){
+					y = origin.getPosY()+positions.get(i).getPosY() - 20;
+				}
+
 				g2.setColor(Color.WHITE);
 				g2.setFont(new Font("Calibri", Font.BOLD, 16));
-				g2.drawString("Klasse " + (i+1), x, y);
+				g2.drawString("K " + (i+1), x, y);
 			}
 			
+			//X-AXIS - Label
+			g2.setColor(Color.BLACK);
+			g2.setFont(new Font("Calibri", Font.BOLD, 16));
+			g2.drawString("Klassenbreite ", origin.getPosX()+chartWidth-50, origin.getPosY()+chartHeight+30);
 			
-			//X-AXIS - Labels
+			
+			//Y-AXIS - Label
 			g2.setColor(Color.BLACK);
 			g2.setFont(new Font("Calibri", Font.BOLD, 16));
 			g2.drawString("r(H) ", origin.getPosX()-35, origin.getPosY()+10);
 			
-			//Regular Labels
-			for(int i=0; i<rectangles.size();i++){
-				int x = origin.getPosX()-12;
-				int y = origin.getPosY()+positions.get(i).getPosY();
-				g2.drawLine(x, y, xTarget, yTarget);
+			//X-AXIS- Regular Labels
+			markerStep = chartWidth/11;
+			for(int i=1; i<11;i++){
+				String labelString = String.valueOf(markerStep*i);
+				int stringWidth = g2.getFontMetrics().stringWidth(labelString);
+				int x = origin.getPosX()+ markerStep*i-stringWidth/2;
+				int y = origin.getPosY()+chartHeight+ 15;
+				g2.setColor(Color.BLACK);
 				g2.setFont(new Font("Calibri", Font.BOLD, 10));
-				g2.drawString("Klasse " + (i+1), x, y);
-				
+				g2.drawString(String.valueOf(labelString), x, y);
 			}
-			//Column Labels
 			
-			//Y-AXIS - Labels
-			g2.setColor(Color.BLACK);
-			g2.setFont(new Font("Calibri", Font.BOLD, 16));
-			g2.drawString("Klassenbreite ", origin.getPosX()+chartWidth-50, origin.getPosY()+chartHeight+25);
+			//Y-AXIS- Regular Labels
+			markerStep = chartHeight/11;
+			for(int i=1; i<11;i++){
+				String labelString = String.format("%.2f", HistogramDataHandler.getMaxHeight(MainFrame.getDataHandler().getList(), MainFrame.getDataHandler().getSampleSize())/10 *i);
+				int x = origin.getPosX()-35;
+				int y = origin.getPosY()+chartHeight-markerStep*i;
+				g2.setColor(Color.BLACK);
+				g2.setFont(new Font("Calibri", Font.BOLD, 10));
+				g2.drawString(String.valueOf(labelString), x, y);
+			}
 			
-			//Regular Labels
-			//Column Labels
 		}
 		
 		//Y-AXIS - LINES
